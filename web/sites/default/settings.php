1dc670a5dcbdb9c07be9fa339450d2f61459ff4a
Max Pogonowski <max@sitback.com.au>
2024-05-02 20:47:24 +1000

    Install vite

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -861,7 +861,6 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
  *
  * Keep this code block at the end of this file to take full effect.
  */
-#
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
