77edab56206dd9652f23188e52f34e8b830d3888
Max Pogonowski <max@sitback.com.au>
2024-05-30 09:17:04 +1000

    Read *.foxy.yml files for JS entry points

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,48 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import { globSync } from 'glob';
+import jsYaml from 'js-yaml';
+const fs = require('fs');
+
+// Function to extract JS files from a given YAML object
+function extractJsFiles(data) {
+  let jsFiles = [];
+
+  function traverse(obj) {
+    if (obj && typeof obj === 'object') {
+      Object.keys(obj).forEach(key => {
+        if (key === 'js' && typeof obj[key] === 'object') {
+          jsFiles.push(...Object.keys(obj[key]));
+        } else {
+          traverse(obj[key]);
+        }
+      });
+    }
+  }
+
+  traverse(data);
+  return jsFiles;
+}
 
 export default defineConfig(({ mode }) => {
   const env = mode === 'production' ? '"production"' : '"development"';
 
-  const test = globSync('./web/modules/**/*.libraries.yml')
-  console.log(test)
+  // Use glob to find all relevant YAML files
+  const files = globSync('./web/modules/**/*.foxy.yml');
 
-  const inputs = globSync('./web/modules/**/entry.js').reduce((acc, input) => {
-    const library = input.match(/\/([^\/]+)\/entry\.js$/)[1];
-    acc[library] = resolve(__dirname, input);
-    return acc;
-  }, {});
+  // Use reduce to process each file and collect all JS files
+  const inputs = files.reduce((acc, libraryFilePath) => {
+    try {
+      const moduleDirectoryPath = libraryFilePath.substring(0, libraryFilePath.lastIndexOf('/'));
+      const fileContents = fs.readFileSync(libraryFilePath, 'utf8');
+      const data = jsYaml.load(fileContents);
+      const jsFiles = extractJsFiles(data);
+      const jsFilesFull = jsFiles.map(x => moduleDirectoryPath + '/' + x);
+      return acc.concat(jsFilesFull);
+    } catch (err) {
+      console.error(`Error processing file ${libraryFilePath}:`, err);
+      return acc;
+    }
+  }, []);
 
   return {
     build: {
