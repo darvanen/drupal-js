597ac898d26dfa3fff1d5f96c4ef2025eb589693
Max Pogonowski <max@sitback.com.au>
2024-05-30 10:06:11 +1000

    Separate compiled libraries into module folders

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,12 @@
-import { defineConfig } from 'vite';
-import { globSync } from 'glob';
-import jsYaml from 'js-yaml';
-const fs = require('fs');
-
-// Function to extract JS files from a given YAML object
-function extractJsFiles(data) {
-  let jsFiles = [];
-
-  function traverse(obj) {
-    if (obj && typeof obj === 'object') {
-      Object.keys(obj).forEach(key => {
-        if (key === 'js' && typeof obj[key] === 'object') {
-          jsFiles.push(...Object.keys(obj[key]));
-        } else {
-          traverse(obj[key]);
-        }
-      });
-    }
-  }
-
-  traverse(data);
-  return jsFiles;
-}
+import { defineConfig } from 'vite'
+import { globSync } from 'glob'
+import jsYaml from 'js-yaml'
+import fs from 'fs'
 
 export default defineConfig(({ mode }) => {
-  const env = mode === 'production' ? '"production"' : '"development"';
+  const env = mode === 'production' ? '"production"' : '"development"'
 
-  // Use glob to find all relevant YAML files
-  const files = globSync('./web/modules/**/*.foxy.yml');
-
-  // Use reduce to process each file and collect all JS files
-  const inputs = files.reduce((acc, libraryFilePath) => {
-    try {
-      const moduleDirectoryPath = libraryFilePath.substring(0, libraryFilePath.lastIndexOf('/'));
-      const fileContents = fs.readFileSync(libraryFilePath, 'utf8');
-      const data = jsYaml.load(fileContents);
-      const jsFiles = extractJsFiles(data);
-      const jsFilesFull = jsFiles.map(x => moduleDirectoryPath + '/' + x);
-      return acc.concat(jsFilesFull);
-    } catch (err) {
-      console.error(`Error processing file ${libraryFilePath}:`, err);
-      return acc;
-    }
-  }, []);
+  const inputs = getInputs()
 
   return {
     build: {
@@ -62,5 +26,55 @@ export default defineConfig(({ mode }) => {
     },
     css: { devSourcemap: true },
     define: { 'process.env.NODE_ENV': env },
-  };
-});
+  }
+})
+
+/**
+ * Gets an array of input file paths.
+ */
+function getInputs() {
+  // Use glob to find all relevant YAML files
+  const files = globSync('./web/modules/**/*.foxy.yml')
+
+  // Use reduce to process each file and collect all JS files
+  return files.reduce((acc, libraryFilePath) => {
+    try {
+      const moduleDirectoryPath = libraryFilePath.substring(0, libraryFilePath.lastIndexOf('/'))
+      const moduleName = moduleDirectoryPath.substring(moduleDirectoryPath.lastIndexOf('/') + 1)
+      const fileContents = fs.readFileSync(libraryFilePath, 'utf8')
+      const data = jsYaml.load(fileContents)
+      const jsFiles = extractJsFiles(data)
+      jsFiles.forEach(filePath => {
+        const fileName = filePath.substring(filePath.lastIndexOf('/') + 1, filePath.lastIndexOf('.'))
+        const key = moduleName + '/' + fileName
+        acc[key] = moduleDirectoryPath + '/' + filePath
+      })
+      return acc
+    } catch (err) {
+      console.error(`Error processing file ${libraryFilePath}:`, err)
+      return acc
+    }
+  }, {})
+}
+
+/**
+ * Extracts JS files from a given YAML object.
+ */
+function extractJsFiles(data) {
+  let jsFiles = []
+
+  function traverse(obj) {
+    if (obj && typeof obj === 'object') {
+      Object.keys(obj).forEach(key => {
+        if (key === 'js' && typeof obj[key] === 'object') {
+          jsFiles.push(...Object.keys(obj[key]))
+        } else {
+          traverse(obj[key])
+        }
+      })
+    }
+  }
+
+  traverse(data)
+  return jsFiles
+}
